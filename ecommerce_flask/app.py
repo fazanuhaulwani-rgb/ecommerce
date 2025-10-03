6383007ef894b06391c537c07a168b5bb7564b84
fazanuhaulwani-rgb <fazanuhaulwani@gmail.com>
2025-10-03 22:27:55 +0700

    Update app.py

--- ecommerce_flask/app.py
+++ ecommerce_flask/app.py
@@ -426,4 +426,4 @@ def sport_detail(slug):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000)) 
-        app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
